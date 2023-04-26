813351875932747b9c7072781f1a6e3ebd796df7
Baraa Aljabban <baraa.aljabban.it@gmail.com>
2023-04-26 14:44:23 +0800

    adding use case to fetch jobs and repo implementation

--- lib/features/available_jobs/data/repositories/available_jobs_repository_impl.dart
+++ lib/features/available_jobs/data/repositories/available_jobs_repository_impl.dart
@@ -1,12 +1,27 @@
 import 'package:dartz/dartz.dart';
+
 import 'package:seekmax/core/errors/error_handler.dart';
-import 'package:seekmax/features/available_jobs/domain/entities/job.dart';
 import 'package:seekmax/core/errors/failures.dart';
+import 'package:seekmax/features/available_jobs/data/datasources/available_jobs_remote_data_souce.dart';
+import 'package:seekmax/features/available_jobs/domain/entities/job.dart';
 import 'package:seekmax/features/available_jobs/domain/repositories/available_jobs_repository.dart';
 
 class AvailableJobsRepositoryImpl extends AvailableJobsRepository with CustomErrorHandler {
+  final AvailableJobsRemoteDataSource remoteDataSource;
+  AvailableJobsRepositoryImpl({
+    required this.remoteDataSource,
+  });
   @override
-  Future<Either<Failure, List<Job>>> fetchAvailableJobs() {
-    throw UnimplementedError();
+  Future<Either<Failure, List<Job>>> fetchAvailableJobs() async {
+    try {
+      final loginResponse = await remoteDataSource.fetchAvailableJobs();
+      return right(loginResponse.data.jobs.jobs);
+    } catch (e) {
+      if (e is Exception) {
+        return Left(mapCommonExceptionToFailure(e));
+      }
+
+      return Left(UnhandledFailure(message: e.toString()));
+    }
   }
 }
